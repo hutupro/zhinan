ac31b3443d89f9c7ee770add803707d372b0f816
hutupro <mail@hutu.pro>
2022-10-07 22:48:23 +0800

    [feature]链接跳转去除referrer信息

--- zhinan-web/src/main.js
+++ zhinan-web/src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Meta from 'vue-meta'
 
 Vue.config.productionTip = false;
+Vue.use(Meta);
 
 new Vue({
   render: h => h(App),
+  metaInfo() {
+    return {
+      meta: [
+        {
+          name: "referrer",
+          content: "no-referrer"
+        }
+      ]
+    }
+  }
 }).$mount('#app');
